efaeaefeace1ae3d7782a4a7c5b577d44f81f43b
Bishnu Mishra <mishravishnu369@gmail.com>
2024-03-19 22:00:13 +0530

    added middleware in auth.route.js file

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,9 +5,12 @@
 */
 
 const authController = require("../controllers/auth.controller");
+const authMW = require("../middleware/auth.mw");
 
 module.exports = (app) => {
     // When the route accepts a POST call for the URI "/ecomm/api/v1/auth/signup",
-    // call signup method from authController module.
-    app.post("/ecomm/api/v1/auth/signup", authController.signup);
+    // call the middleware function named verifySignupBody from authMW module.
+    // Post the middleware completes its task, call the next route handler function.
+    // Here, that next route handler function is the signup method from authController module.
+    app.post("/ecomm/api/v1/auth/signup", [authMW.verifySignupBody], authController.signup);
 }
