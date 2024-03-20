f452a9be62dfca8326b13a823118bdf4bb8a0d50
Bishnu Mishra <mishravishnu369@gmail.com>
2024-03-20 22:37:10 +0530

    added sign-in functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,8 @@
 
 const bcrypt = require("bcryptjs");
 const user_model = require("../models/user.model");
+const secret = require("../configs/auth.config");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
     // To create a new user, we follow a simple 3-step process:
@@ -41,4 +43,59 @@ exports.signup = async (req, res) => {
             message : "Some error happened while registering the user."
         });
     }
+}
+
+exports.signin = async (req, res) => {
+    // retrieve user by using their userId from request body
+    const user = await user_model.findOne({userId: req.body.userId});
+
+    // if user is not present, send a response accordingly
+    if(user == null) {
+        return res.status(401).send({
+            message: "This userId does not exists."
+        });
+    }
+
+    // if userId is present, check if they passed the correct password or not
+    const isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
+    //  The bcrypt.compareSync() function is used to compare a plain text password
+    // (typically provided by a user during authentication) with a hashed password
+    // stored in a database. If both matches, this function returns true, else false.
+    // The first parameter should be the plain text password, and the second parameter
+    // is the hashed password retrieved from database.
+
+    if(!isPasswordValid) {
+        return res.status(401).send({
+            message: "Password is not correct."
+        });
+    }
+
+    // If program comes here, means userID and password both were correct.
+    // Thus, generate an access token for the user using JSON Web token(jwt) for the user.
+    const token = jwt.sign({id: user.userId}, secret.secret, {expiresIn: 120});
+    // jwt.sign() function generates a JWT by taking 3 parameters.
+    // 1st param: This is the payload of the JWT, which contains the data(here, userID).
+    //            This data will be encoded into the JWT and can be decoded later to
+    //            retrieve the user's ID.
+    // 2nd param: This is the secret key used to sign the JWT. It's crucial for adding more
+    //            complexity to the generated token. If we do not provide a strong secret key, the
+    //            JWT can be easily decoded using any online jwt decoder tools like https://jwt.io/.
+    //            This could lead hackers potentially create their own JWTs with arbitrary claims or
+    //            manipulate existing JWTs to gain unauthorized access to protected resources.
+    // 3rd param: This is an optional parameter that specifies the expiration time of the token.
+    //            It indicates the amount of time (in seconds) after which the token will expire.
+    //            In this case, the token will expire in 120 seconds (2 minutes) after it is generated.
+    //            After expiration, the token will no longer be considered valid, and the client will
+    //            need to request a new token. Setting an expiration time helps improve security by
+    //            limiting the window of opportunity for potential misuse of the token.
+
+    // All okay, thus send the response object finally for the sign-in functionality
+    res.status(200).send({
+        name: user.name,
+        userId: user.userId,
+        email: user.email,
+        userType: user.userType,
+        accessToken: token
+    });
+
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
-/**
- * POST localhost:8888/ecomm/api/v1/auth/signup
- * 
- * I need to intercept this
+/*
+ * POST localhost:8000/ecomm/api/v1/auth/signup
+ * POST localhost:8000/ecomm/api/v1/auth/signin
+ * I need to intercept the above two REST API end-points. This is
+ * example of an endpoint: 'localhost:/ecomm/api/v1/auth/signup'.
 */
 
 const authController = require("../controllers/auth.controller");
@@ -12,5 +13,7 @@ module.exports = (app) => {
     // call the middleware function named verifySignupBody from authMW module.
     // Post the middleware completes its task, call the next route handler function.
     // Here, that next route handler function is the signup method from authController module.
+    // Same is valid for signin POST call too.
     app.post("/ecomm/api/v1/auth/signup", [authMW.verifySignupBody], authController.signup);
+    app.post("/ecomm/api/v1/auth/signin", [authMW.verifySignInBody], authController.signin);
 }
